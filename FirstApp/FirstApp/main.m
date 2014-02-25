cd1b222b5de3dd728fb751580be63122f472593d
mike stromme <mike.stromme@breakthroughfuel.com>
2014-02-24 21:40:03 -0600

    first commit

---
+++ FirstApp/FirstApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Michael Stromme on 2/22/14.
+//  Copyright (c) 2014 mikestromme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
